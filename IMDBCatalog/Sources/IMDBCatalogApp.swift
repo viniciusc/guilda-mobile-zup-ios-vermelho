8b8edd4e5d1059b5f8e656853a907ecd2fa4785e
ewerton pereira <eaap2007@gmail.com>
2024-05-21 08:58:05 -0300

    first commit

---
+++ IMDBCatalog/Sources/IMDBCatalogApp.swift
@@ -0,0 +1,20 @@
+//
+//  IMDBCatalogApp.swift
+//
+
+import SwiftUI
+
+@main
+struct IMDBCatalogApp: App {
+
+    init() {
+        
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Testando")
+        }
+    }
+    
+}
